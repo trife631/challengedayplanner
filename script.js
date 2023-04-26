bd5c1a395baf3cc9ca540977bbd5cf1bea5fe039
trife631 <126350708+trife631@users.noreply.github.com>
2023-04-26 18:41:23 -0400

    day planner second commits

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 $(function () {
+ var saveBtn = $('.saveBtn')
+
+
+ var date = dayjs().format("D MMM YYYY")
+ $('#curentDay').text(date)
  
-});
+var hour = dayjs().hour()
+var block = $(".time-block")
+
+block.each(function() {
+  var hourId = $(this).attr("id")
+
+  if(hourId < hour) {
+    $(this).children(".col-8").attr("class", "col-8 col-md-10 description past")
+  }
+  if(hourId == hour) {
+    $(this).children(".col-8").attr("class", "col-8 col-md-10 description present")
+  }
+  if(hourId > hour) {
+    $(this).children(".col-8").attr("class", "col-8 col-md-10 description future")
+  }
+
+  saveBtn.on("click", function(event) {
+    event.preventDefault()
+    var key = $(this).parents().attr("id")
+    var value = $(this).siblings(".col-8").val().replace(key)
+
+    localStorage.setItem(key, JSON.stringify(value))
+  })
+
+})
+
+})
