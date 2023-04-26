76689038e7677bdd05535fd814457d3136d7b91a
trife631 <126350708+trife631@users.noreply.github.com>
2023-04-26 18:43:55 -0400

    day planner third commits

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 $(function () {
- var saveBtn = $('.saveBtn')
+  var saveBtn = $('.saveBtn')
 
 
- var date = dayjs().format("D MMM YYYY")
- $('#curentDay').text(date)
- 
-var hour = dayjs().hour()
-var block = $(".time-block")
+  var date = dayjs().format("D MMM YYYY")
+  $('#curentDay').text(date)
 
-block.each(function() {
-  var hourId = $(this).attr("id")
+  var hour = dayjs().hour()
+  var block = $(".time-block")
 
-  if(hourId < hour) {
-    $(this).children(".col-8").attr("class", "col-8 col-md-10 description past")
-  }
-  if(hourId == hour) {
-    $(this).children(".col-8").attr("class", "col-8 col-md-10 description present")
-  }
-  if(hourId > hour) {
-    $(this).children(".col-8").attr("class", "col-8 col-md-10 description future")
-  }
+  block.each(function () {
+    var hourId = $(this).attr("id")
 
-  saveBtn.on("click", function(event) {
+    if (hourId < hour) {
+      $(this).children(".col-8").attr("class", "col-8 col-md-10 description past")
+    }
+    if (hourId == hour) {
+      $(this).children(".col-8").attr("class", "col-8 col-md-10 description present")
+    }
+    if (hourId > hour) {
+      $(this).children(".col-8").attr("class", "col-8 col-md-10 description future")
+    }
+  })
+
+  saveBtn.on("click", function (event) {
     event.preventDefault()
     var key = $(this).parents().attr("id")
     var value = $(this).siblings(".col-8").val().replace(key)
@@ -29,6 +30,7 @@ block.each(function() {
     localStorage.setItem(key, JSON.stringify(value))
   })
 
-})
-
+  for (let i = 9; i <= 17; i++) {
+    $(`#${i} textarea`).val(JSON.parse(localStorage.getItem(`${i}`)))
+  }
 })
